1003edaaaa3e5a38a87c0cc1ef5cac1a565f8ede
Anak Wannaphaschaiyong <awannaphasch2016@fau.edu>
2021-02-21 08:57:30 -0500

    2/21/2021 update

---
+++ Scripts/Setup/.dotfiles/Ubuntu/setup_file_dirs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ~
+mkdir  ~/Downloads
+mkdir  ~/Scripts
+mkdir  ~/Scratches
+mkdir  ~/Data
+mkdir  -p ~/Documents/Working
+mkdir  ~/Documents/Courses
+
+# cloneing github repo
+mkdir  ~/Documents
+git clone https://github.com/Awannaphasch2016/MyNotes.git
+git clone https://github.com/Awannaphasch2016/AWSCovid19SocialMediaCralwer.git
+git clone https://github.com/Awannaphasch2016/TODO-Manager
+git clone https://github.com/Awannaphasch2016/AnakBlog
+git clone https://github.com/Awannaphasch2016/PaperSummary
+git cloen https://github.com/Awannaphasch2016/COVID19TrendPrediction
